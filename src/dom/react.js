e2e880eb5944e8b9b5e0f508f15d20c44bf1a2a4
段昊辰 <duanhaochen@126.com>
2018-07-03 16:49:36 +0800

    ref兼容旧版本的react

--- src/dom/react.js
+++ src/dom/react.js
@@ -14,8 +14,12 @@ class TextDifferentForReact extends Component{
     type: ''
   };
 
-  oldCodeRef: Object = createRef();
-  newCodeRef: Object = createRef();
+  oldCodeRef: Object | Function = createRef
+    ? createRef()
+    : (element: Element): { current: Element } => this.oldCodeRef = { current: element };
+  newCodeRef: Object | Function = createRef
+    ? createRef()
+    : (element: Element): { current: Element } => this.newCodeRef = { current: element };
 
   componentDidMount(): void{
     init(this.props.oldCode, this.props.newCode, this.oldCodeRef.current, this.newCodeRef.current);
